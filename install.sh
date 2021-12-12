c2da699a1b915b5ef3eeb94a37bc7a81fdee252c
K4L1T00lz <95284550+K4L1T00lz@users.noreply.github.com>
2021-12-12 13:38:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo apt-get install -qq xterm  && echo Installing dependencies....
 sleep 1
-sudo xterm -e wget 0.0.0.0/test.py && sudo xterm -e sudo apt-get install -y build-essential autoconf automake libtool pkg-config libnl-3-dev libnl-genl-3-dev libssl-dev ethtool shtool rfkill zlib1g-dev libpcap-dev libsqlite3-dev libpcre3-dev libhwloc-dev libcmocka-dev hostapd wpasupplicant tcpdump screen iw usbutils git ruby default-jdk python3-pip python3-venv alien devscripts && echo Installed
+sudo xterm -e wget https://raw.githubusercontent.com/K4L1T00lz/K4L1T00lz/main/K4L1T00lz-cli.py && sudo xterm -e sudo apt-get install -y build-essential autoconf automake libtool pkg-config libnl-3-dev libnl-genl-3-dev libssl-dev ethtool shtool rfkill zlib1g-dev libsqlite3-dev libpcre3-dev libhwloc-dev libcmocka-dev hostapd wpasupplicant tcpdump screen iw usbutils git ruby default-jdk python3-pip python3-venv alien devscripts golang libpcap-dev libusb-1.0-0-dev libnetfilter-queue-dev
+&& echo Dependencies Installed
 sleep 1
 echo Running the script.....
 sleep 1
-python3 test.py
-
+sudo python3 K4L1T00lz-cli.py
